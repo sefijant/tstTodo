02371b30815cdfbb05a0e91cbf0532eda05b1684
sefi <sefi007777@gmail.com>
2018-03-24 01:07:03 +0300

    Tons of new stuff...

--- lists.js
+++ lists.js
@@ -1,29 +1,100 @@
-
 var app = angular.module("ToDo", []);
 app.controller("MainController", function ($scope, $http) {
-	$http.get('/Lists/travel').then(function(response) {
+    $http.get('/Lists/travel').then(function (response) {
         $scope.travelLists = response.data;
     });
-	$http.get('/Lists/army').then(function(response) {
+    $http.get('/Lists/army').then(function (response) {
         $scope.armyLists = response.data;
     });
-	$http.get('/Lists/shopping').then(function(response) {
+    $http.get('/Lists/shopping').then(function (response) {
         $scope.shoppingLists = response.data;
     });
-	$http.get('/Lists/other').then(function(response) {
+    $http.get('/Lists/other').then(function (response) {
         $scope.otherLists = response.data;
     });
-	var categories = [{ "name": "קניות", "image": ".\\resources\\shopping.jpg", "modal": "#shopping" }, { "name": "צבא", "image": ".\\resources\\army.jpg", "modal": "#army" }, { "name": "טיולים", "image": ".\\resources\\travel.jpg", "modal": "#travel" }, { "name": "אחר", "image": ".\\resources\\else.jpg", "modal": "#other" }];
-    $scope.categories = categories;
+    $http.get('/Categories/').then(function (response) {
+        $scope.categories = response.data;
+    });
+
+    $scope.plusImage = "\\resources\\plus.png";
+    $scope.plusModal = "#newList";
+    $scope.userInputtedList = {
+        "name": "",
+        "category": "",
+        "items": [],
+        "uses": 0
+    };
+    $scope.newListItem = "";
 
     (function ($) {
         $(function () {
-    
             //initialize all modals           
             $('.modal').modal();
-    
         }); // end of document ready
     })(jQuery);
+    var config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        }
+    };
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+    );
 
+    $scope.onAddItemKeyPress = function (event) {
+        if (event.charCode == 13) {
+            $scope.userInputtedList.items.push($scope.newListItem);
+            $scope.newListItem = "";
+        } //if enter then hit the search button
+    }
+
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`
+        $scope.userInputtedList = {
+            "name": "",
+            "category": "",
+            "items": [],
+            "uses": 0
+        };
+        $scope.newListItem = "";
+       }
+   });
+
+    $scope.addListToDB = function () {
+        $http.post('/addList/', $scope.userInputtedList)
+            .then(
+            function (response) {
+                console.log(response);
+            }
+            );
+        $scope.userInputtedList = {
+            "name": "",
+            "category": "",
+            "items": [],
+            "uses": 0
+        };
+        $scope.newListItem = "";
+    };
+
+
+    var trying = { "items": ["2", "כdfbdfי"], "name": "נסיון", "category": "other", "uses": 3 };
+    $http.post('/updateList/', trying)
+        .then(
+        function (response) {
+        }
+        );
+    $http.post('/boom/', trying)
+        .then(
+        function (response) {
+        }
+        );
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var cors = require('cors');
 var Promise = require('bluebird');
 var rp = require('request-promise');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 
 var ObjectId = require('mongodb').ObjectID;
 const mongoURL = "mongodb://todobomdb:TuzLJr7l4azXSM3Z06DzFZfe3MPg4DErOF9cFNxsNv9LV5lsUAjych7VC6lj5YZS4GpUplf4huBkHOSSrZLTNQ==@todobomdb.documents.azure.com:10255/?ssl=true";
@@ -15,46 +22,138 @@ var ListsSchema = new mongoose.Schema({
         category: String,
         items: [String],
         uses: Number
-        }]
+    }]
 });
+
+var ListSchema = new mongoose.Schema({
+    name: String,
+    category: String,
+    items: [String],
+    uses: Number
+});
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    modal: String
+});
+
 var Model = mongoose.model('lists', ListsSchema);
+var listModel = mongoose.model('list', ListSchema);
+var Category = mongoose.model('categories', catSchema);
 
 
-// Connect to mongoDB database assuming our database name is "angular-app"
-// const mongoURL = 'mongodb://localhost/angular-app';
- //mongoose.connect(mongoURL);
-
- mongoose.connect('mongodb://todobomdb.documents.azure.com:10255/ToDoBom?ssl=true', {
+mongoose.connect('mongodb://todobomdb.documents.azure.com:10255/ToDoBom?ssl=true', {
     auth: {
-      user: 'todobomdb',
-      password: 'TuzLJr7l4azXSM3Z06DzFZfe3MPg4DErOF9cFNxsNv9LV5lsUAjych7VC6lj5YZS4GpUplf4huBkHOSSrZLTNQ=='
+        user: 'todobomdb',
+        password: 'TuzLJr7l4azXSM3Z06DzFZfe3MPg4DErOF9cFNxsNv9LV5lsUAjych7VC6lj5YZS4GpUplf4huBkHOSSrZLTNQ=='
     }
-  })
-  .then(() => console.log('connection successful'))
-  .catch((err) => console.error(err));
-  mongoose.Promise = global.Promise;
+})
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
+mongoose.Promise = global.Promise;
 
 app.use(express.static(__dirname + '/'));
 // viewed at http://localhost:8080
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.get('/Lists/:query', cors(), function(req, res) {    
-        var query = req.params.query;
-		Model.find({
-			'category': query
-		}, function(err, result) {
+app.get('/Lists/:query', cors(), function (req, res) {
+    var query = req.params.query;
+    Model.find({
+        'category': query
+    },
+        null,
+        {
+            sort: {
+                uses: -1
+            }
+        },
+        function (err, result) {
             if (err) throw err;
             if (result) {
                 res.json(result)
             } else {
                 res.send(JSON.stringify({
-                    error : 'Error'
+                    error: 'Error'
                 }))
             }
         })
-    });
+});
 
+app.get('/Lists/', cors(), function (req, res) {
+    Model.find({}, function (err, result) {
+        if (err) throw err;
+        if (result) {
+            res.json(result)
+        } else {
+            res.send(JSON.stringify({
+                error: 'Error'
+            }))
+        }
+    })
+});
+
+app.get('/Categories/', cors(), function (req, res) {
+    Category.find({}, function (err, result) {
+        if (err) throw err;
+        if (result) {
+            res.json(result)
+        } else {
+            res.send(JSON.stringify({
+                error: 'Error'
+            }))
+        }
+    })
+});
+
+app.post('/getList/', function (req, res, next) {
+    listModel.findOne({ name: req.body.name }, function (err, result) {
+        if (err) throw err;
+        if (result) {
+            res.json(result)
+        } else {
+            res.send(JSON.stringify({
+                error: 'Error'
+            }))
+        }
+    })
+});
+
+app.post('/addList/', function (req, res, next) {
+    var list = new listModel({
+        name: req.body.name,
+        category: req.body.category,
+        items: req.body.items,
+        uses: req.body.uses
+    })
+    list.save(function (err, list) {
+        if (err) { return next(err) }
+        res.json(201, list)
+    })
+});
+
+app.post('/updateList/', function (req, res, next) {
+    var list = new listModel({
+        name: req.body.name,
+        category: req.body.category,
+        items: req.body.items,
+        uses: req.body.uses
+    })
+    listModel.update({ name: req.body.name }, { name: req.body.name, category: req.body.category, items: req.body.items, uses: req.body.uses }, { upsert: true, safe: true }, function (err, list) {
+        if (err) { return next(err) }
+        res.json(list)
+    })
+
+});
+
+app.post('/boom/', function (req, res, next) {
+    listModel.update({ name: req.body.name }, { uses: req.body.uses + 1 }, { upsert: true, safe: true }, function (err, list) {
+        if (err) { return next(err) }
+        res.json(list)
+    })
+
+});
 app.listen(80);
 
- 
+
